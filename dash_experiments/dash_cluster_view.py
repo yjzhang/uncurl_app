94b1f862c88769c59b112f7d27e36e3c7981f743
Yue Zhang <yjzhang@cs.washington.edu>
2018-02-18 04:04:24 -0800

    dash plot looks better

--- dash_experiments/dash_cluster_view.py
+++ dash_experiments/dash_cluster_view.py
@@ -9,7 +9,31 @@ import plotly.graph_objs as go
 
 import numpy as np
 
-app = dash.Dash()
+app = dash.Dash(name='Cluster view', sharing=True)
+app.layout = html.Div(
+        id='app-layout',
+        children=[
+            dcc.Location(id='url', refresh=False),
+            html.Div(id='page-content', className='container')
+        ]
+    )
+
+# code based on https://github.com/plotly/dash/issues/38#issuecomment-364927379
+def router(pathname, **kwargs):
+    """
+    pathname is of the form <test or id>/<uuid>
+    """
+    _ = pathname.split('/')
+    resource = _[1]
+    resource_id = _[2]
+    if resource == 'test':
+        return initialize(os.path.join('test', resource_id))
+    else:
+        return initialize(os.path.join('/tmp/uncurl', resource_id))
+
+@app.callback(Output('page-content', 'children'), [Input('url', 'pathname')])
+def display_page(pathname):
+    return router(pathname)
 
 def generate_cluster_view(M, dim_red, top_genes, n_genes=10):
     """
@@ -18,7 +42,7 @@ def generate_cluster_view(M, dim_red, top_genes, n_genes=10):
     """
     return html.Div([
         # view 1: graph plot
-        dcc.Graph(id='means',
+        html.Div(dcc.Graph(id='means',
             figure={
                 'data': [
                     go.Scatter(
@@ -27,7 +51,7 @@ def generate_cluster_view(M, dim_red, top_genes, n_genes=10):
                         mode='markers',
                         text=['cluster ' + str(x) for x in range(dim_red.shape[1])],
                         marker={
-                            'size': 50
+                            'size': 20
                         },
                     ),
                 ],
@@ -36,9 +60,10 @@ def generate_cluster_view(M, dim_red, top_genes, n_genes=10):
                     xaxis={'title': 'dim1'},
                     yaxis={'title': 'dim2'},
                 ),
-            }),
+                }, style={'width': 700}),
+            style={'display': 'inline-block'}),
         # view 2: top genes
-        dcc.Graph(id='top-genes',
+        html.Div(dcc.Graph(id='top-genes',
             figure={
                 'data': [
                     go.Bar(
@@ -46,23 +71,34 @@ def generate_cluster_view(M, dim_red, top_genes, n_genes=10):
                         y=[1,2,3],
                         orientation='h'
                     )],
-                'layout': {
-                    'title': 'Top genes',
-                }
-            }),
-    ])
+                'layout': go.Layout(
+                    title='Cluster means',
+                    xaxis={'title': 'dim1'},
+                    yaxis={'title': 'dim2'},
+                ),
+                }, style={'width': 400}),
+            style={'display': 'inline-block'}),
+        ], style={'width': '100%', 'display':'inline-block'})
 
 
+# TODO: dynamically generate an app given a path???
 def initialize(data_dir=None):
     M = None
-    mds_output = np.array([[1,2,3,4],[1,2,3,4]])
-    top_genes = {0: [(0,100),(1,50),(2,40)], 1: [(0,50),(1,45),(2,30)]}
+    mds_means = np.array([[1,2,3,4],[1,2,3,4]])
+    mds_data = None
+    top_genes = {'0': [(0,100),(1,50),(2,40)], '1': [(0,50),(1,45),(2,30)]}
+    gene_names = None
     if data_dir != None:
         M = np.loadtxt(os.path.join(data_dir, 'm.txt'))
-        mds_output = np.loadtxt(os.path.join(data_dir, 'mds.txt'))
+        mds_means = np.loadtxt(os.path.join(data_dir, 'mds_means.txt'))
+        mds_data = np.loadtxt(os.path.join(data_dir, 'mds_data.txt'))
         with open(os.path.join(data_dir, 'top_genes.txt')) as f:
             top_genes = json.load(f)
-    app.layout = generate_cluster_view(M, mds_output, top_genes)
+        try:
+            gene_names = np.loadtxt(os.path.join(data_dir, 'gene_names.txt'), dtype=str)
+        except:
+            gene_names = np.array([str(x) for x in range(len(mds_data.shape[1]))])
+    app.layout = generate_cluster_view(M, mds_means, top_genes)
 
     # create callback for clicking on clusters
     @app.callback(
@@ -73,25 +109,28 @@ def initialize(data_dir=None):
         print('input value:')
         print(input_value)
         if input_value is None:
-            input_value = 0
+            input_value = '0'
         else:
-            input_value = input_value['points'][0]['pointIndex']
+            input_value = str(input_value['points'][0]['pointIndex'])
         selected_top_genes = top_genes[input_value][:10]
+        selected_gene_names = [gene_names[x[0]] for x in selected_top_genes]
         return {
                 'data': [
                     go.Bar(
-                        y=[str(x[0]) for x in selected_top_genes],
+                        y=selected_gene_names,
                         x=[x[1] for x in selected_top_genes],
                         orientation='h',
                     )
                 ],
-                'layout': {
-                    'title': 'Top genes',
-                }
+                'layout': go.Layout(
+                    title='Top genes for cluster {0}'.format(input_value),
+                    xaxis={'title': 'c-score'},
+                    yaxis={'title': 'genes'},
+                    autosize=False,
+                ),
         }
-        #return 'You\'ve entered "{0}"'.format(input_value)
 
 
 if __name__ == '__main__':
-    initialize()
+    initialize('../data/test1_output')
     app.run_server()
---
+++ dash_experiments/generate_analysis.py
@@ -0,0 +1,73 @@
+# This generates uncurl analyses as static HTML files.
+# inputs: raw data (matrix or file), M (or file), W (or file), reduced_data (2d for vis)
+import json
+import os
+
+import numpy as np
+import scipy.io
+from scipy import sparse
+import uncurl
+import uncurl_analysis
+
+def generate_uncurl_analysis(data, output_dir,
+        data_type='dense',
+        gene_names=None,
+        gene_sub=True,
+        **uncurl_kwargs):
+    """
+    Performs an uncurl analysis of the data, writing the results in the given
+    directory.
+
+    Outputs:
+        output_dir/m.txt
+        output_dir/w.txt
+        output_dir/top_genes.txt (json of a dict mapping cluster ids to a list of (gene_id : c_score) sorted by c_score)
+        output_dir/mds_means.txt (mds of the means)
+        output_dir/mds_data.txt (mds projection of data)
+        output_dir/gene_subset.txt (gene subset selected by uncurl)
+        output_dir/gene_names.txt (list of all gene names in data subset)
+
+    Args:
+        data (array or str): either a data array, or a string containing
+            the path to a data array..
+        output_dir (str): directory to write output to.
+        data_type (str): if data is a path, this indicates whether the data is a dense or sparse array.
+        gene_names (list or array): list of all gene names
+        gene_sub (bool): whether or not to use gene subset selection (max_variance_genes)
+        **uncurl_kwargs: arguments to pass to uncurl.run_state_estimation. has to include clusters=k.
+    """
+    try:
+        os.makedirs(output_dir)
+    except:
+        print('could not make output dir: {0}'.format(output_dir))
+    if isinstance(data, str):
+        if data_type == 'dense':
+            data = np.loadtxt(data)
+        elif data_type == 'sparse':
+            data = scipy.io.mmread(data)
+            data = sparse.csc_matrix(data)
+    if isinstance(gene_names, str):
+        gene_names = np.loadtxt(gene_names, dtype=str)
+    # run uncurl
+    if gene_sub:
+        genes_subset = np.array(uncurl.max_variance_genes(data))
+        np.savetxt(os.path.join(output_dir, 'gene_subset.txt'), genes_subset, fmt='%d')
+        data = data[genes_subset,:]
+        if gene_names is not None:
+            gene_names = gene_names[genes_subset]
+    print(uncurl_kwargs)
+    m, w, ll = uncurl.run_state_estimation(data, **uncurl_kwargs)
+    np.savetxt(os.path.join(output_dir, 'm.txt'), m)
+    np.savetxt(os.path.join(output_dir, 'w.txt'), w)
+    # find overexpressed genes for clusters
+    top_genes = uncurl_analysis.find_overexpressed_genes(data, w.argmax(0))
+    with open(os.path.join(output_dir, 'top_genes.txt'), 'w') as f:
+        json.dump(top_genes, f)
+    # run mds
+    mds_output = uncurl.dim_reduce(m, w, 2)
+    print(mds_output.shape)
+    np.savetxt(os.path.join(output_dir, 'mds_means.txt'), mds_output.T)
+    mds_data = uncurl.mds(m, w, 2)
+    np.savetxt(os.path.join(output_dir, 'mds_data.txt'), mds_data)
+    if gene_names is not None:
+        np.savetxt(os.path.join(output_dir, 'gene_names.txt'), gene_names, fmt='%s')
