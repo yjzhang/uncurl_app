b064a9452ffe5d8dca4fd7386f00d98ad3f0eadf
Yue Zhang <yjzhang@cs.washington.edu>
2018-02-18 16:40:22 -0800

    added cell visualization in mds

--- dash_experiments/dash_cluster_view.py
+++ dash_experiments/dash_cluster_view.py
@@ -35,32 +35,82 @@ def router(pathname, **kwargs):
 def display_page(pathname):
     return router(pathname)
 
-def generate_cluster_view(M, dim_red, top_genes, n_genes=10):
+def create_means_figure(dim_red, colorscale='Portland'):
     """
-    Generates a cluster view: MDS plot of means on the left, updated bar plot
-    of top genes with c-scores on the right.
+    create a figure for displaying means
     """
-    return html.Div([
-        # view 1: graph plot
-        html.Div(dcc.Graph(id='means',
-            figure={
+    return {
                 'data': [
                     go.Scatter(
-                        x=dim_red[0,:],
-                        y=dim_red[1,:],
+                        x=dim_red[0,c:c+1],
+                        y=dim_red[1,c:c+1],
                         mode='markers',
-                        text=['cluster ' + str(x) for x in range(dim_red.shape[1])],
+                        name='cluster ' + str(c),
                         marker={
-                            'size': 20
+                            'size': 20,
+                            'color': c,
+                            'colorscale': colorscale,
                         },
-                    ),
+                    )
+                    for c in range(dim_red.shape[1])
                 ],
                 'layout': go.Layout(
                     title='Cluster means',
                     xaxis={'title': 'dim1'},
                     yaxis={'title': 'dim2'},
+                    margin={'t':30},
                 ),
-                }, style={'width': 700}),
+        }
+
+def create_cells_figure(dim_red, labels, colorscale='Portland'):
+    """
+    create a figure for displaying cells
+    """
+    return {
+                'data': [
+                    go.Scatter(
+                        x=dim_red[0,labels==c],
+                        y=dim_red[1,labels==c],
+                        mode='markers',
+                        name='cluster ' + str(c),
+                        marker={
+                            'size': 10,
+                            'color': c,
+                            'colorscale': colorscale,
+                        },
+                    )
+                    for c in range(max(labels))
+                ],
+                'layout': go.Layout(
+                    title='Cells',
+                    xaxis={'title': 'dim1'},
+                    yaxis={'title': 'dim2'},
+                    margin={'t':30},
+                ),
+        }
+
+def generate_cluster_view(M, dim_red, top_genes, n_genes=10):
+    """
+    Generates a cluster view: MDS plot of means on the left, updated bar plot
+    of top genes with c-scores on the right.
+    """
+    colorscale = 'Portland'
+    #if dim_red.shape[1] > 10:
+    #    colorscale = 'Jet'
+    return html.Div([
+        html.Div([
+            dcc.RadioItems(
+                id='means-or-cell',
+                options=[{'label': 'Display Means', 'value': 'Means'},
+                         {'label': 'Display Cells', 'value': 'Cells'}],
+                value='Means',
+                labelStyle={'display': 'inline-block'},
+            )
+        ]),
+        # view 1: graph plot
+        html.Div(dcc.Graph(id='means',
+                figure=create_means_figure(dim_red, colorscale),
+                style={'width': 700}),
             style={'display': 'inline-block'}),
         # view 2: top genes
         html.Div(dcc.Graph(id='top-genes',
@@ -84,12 +134,16 @@ def generate_cluster_view(M, dim_red, top_genes, n_genes=10):
 # TODO: dynamically generate an app given a path???
 def initialize(data_dir=None):
     M = None
+    W = None
+    labels = None
     mds_means = np.array([[1,2,3,4],[1,2,3,4]])
     mds_data = None
     top_genes = {'0': [(0,100),(1,50),(2,40)], '1': [(0,50),(1,45),(2,30)]}
     gene_names = None
     if data_dir != None:
         M = np.loadtxt(os.path.join(data_dir, 'm.txt'))
+        W = np.loadtxt(os.path.join(data_dir, 'w.txt'))
+        labels = W.argmax(0)
         mds_means = np.loadtxt(os.path.join(data_dir, 'mds_means.txt'))
         mds_data = np.loadtxt(os.path.join(data_dir, 'mds_data.txt'))
         with open(os.path.join(data_dir, 'top_genes.txt')) as f:
@@ -98,6 +152,8 @@ def initialize(data_dir=None):
             gene_names = np.loadtxt(os.path.join(data_dir, 'gene_names.txt'), dtype=str)
         except:
             gene_names = np.array([str(x) for x in range(len(mds_data.shape[1]))])
+
+    # generate layout
     app.layout = generate_cluster_view(M, mds_means, top_genes)
 
     # create callback for clicking on clusters
@@ -111,7 +167,7 @@ def initialize(data_dir=None):
         if input_value is None:
             input_value = '0'
         else:
-            input_value = str(input_value['points'][0]['pointIndex'])
+            input_value = str(input_value['points'][0]['curveNumber'])
         selected_top_genes = top_genes[input_value][:10]
         selected_gene_names = [gene_names[x[0]] for x in selected_top_genes]
         return {
@@ -126,9 +182,21 @@ def initialize(data_dir=None):
                     title='Top genes for cluster {0}'.format(input_value),
                     xaxis={'title': 'c-score'},
                     yaxis={'title': 'genes'},
-                    autosize=False,
                 ),
         }
+
+    # create callback for switching to cell view
+    @app.callback(
+        Output(component_id='means', component_property='figure'),
+        [Input(component_id='means-or-cell', component_property='value')]
+    )
+    def update_scatterplot(input_value):
+        print('update_scatterplot')
+        print(input_value)
+        if input_value == 'Means':
+            return create_means_figure(mds_means)
+        elif input_value == 'Cells':
+            return create_cells_figure(mds_data, labels)
 
 
 if __name__ == '__main__':
