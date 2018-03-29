f0d1d3dbd873c69c7193623be7de846a1451a62a
Ubuntu <ubuntu@ip-172-31-6-20.us-west-2.compute.internal>
2018-03-29 22:32:15 +0000

    server start script

--- start_gunicorn.sh
+++ start_gunicorn.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-killall gunicorn
-nohup gunicorn --workers 1 --bind 0.0.0.0:8888 --log-level debug -t 100 wsgi:app &
+killall -9 gunicorn
+nohup gunicorn --workers 1 --threads 1 --bind 0.0.0.0:8888 --log-level debug -t 100 wsgi:app &
