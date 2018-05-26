1af4d1d14812457a39a36ac60f2bde20b886e50b
yjzhang <yjzhang@cs.washington.edu>
2018-05-26 10:08:31 +0000

    increased timeout time

--- start_gunicorn.sh
+++ start_gunicorn.sh
@@ -4,4 +4,4 @@ killall -9 redis-server
 nohup redis-server > nohup_redis.out &
 
 killall -9 gunicorn
-nohup gunicorn --workers 4 --threads 1 --bind 0.0.0.0:8888 --log-level debug -t 500 wsgi:app > nohup_uncurl.out &
+nohup gunicorn --workers 4 --threads 1 --bind 0.0.0.0:8888 --log-level debug -t 5000 wsgi:app > nohup_uncurl.out &
