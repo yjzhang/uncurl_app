6d8bf451336266d2fc529004b92e2014ffd52e57
Yue Zhang <yjzhang@cs.washington.edu>
2019-04-04 19:11:38 -0700

    starting on split_seq input

--- uncurl_app/__init__.py
+++ uncurl_app/__init__.py
@@ -4,6 +4,8 @@ from flask_bootstrap import Bootstrap
 
 from . import interaction_views, views, flask_router
 
+from .cache import cache
+
 def create_app(config_filename=None):
     app = Flask(__name__)
     Bootstrap(app)
@@ -36,9 +38,31 @@ def create_app(config_filename=None):
         app.register_blueprint(flask_router.flask_router)
     return app
 
-def create_app_split_seq(data_dir=None, config_filename=None):
+def create_app_split_seq(data_dir='./', config_filename=None):
     # TODO: create an app starting at the data preprocessing view, where
     # data_dir is the DGE folder output from split-seq.
-    pass
-
-from .cache import cache
+    from split_seq_input import process_split_seq
+    process_split_seq(data_dir)
+    app = Flask(__name__)
+    Bootstrap(app)
+    app.config['MAX_CONTENT_LENGTH'] = 1000 * 1024 * 1024
+    # default args to pass to uncurl.run_state_estimation
+    app.config['UNCURL_ARGS'] = {
+            'threads': 2,
+            'max_iters': 20,
+            'inner_max_iters': 50
+    }
+    app.config['NMF_ARGS'] = {
+    }
+    app.config['TEST_DATA_DIR'] = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
+                    'test_data')
+    # TODO: go up 1 from path
+    app.config['USER_DATA_DIR'] = os.path.dirname(data_dir)
+    app.config['CACHE_TYPE'] = 'simple'
+    cache.config = {'CACHE_TYPE': 'simple'}
+    app.config['SHOW_ALL_RESULTS'] = True
+    with app.app_context():
+        app.register_blueprint(interaction_views.interaction_views)
+        app.register_blueprint(views.views)
+        app.register_blueprint(flask_router.flask_router)
+    return app
---
+++ uncurl_app/split_seq_input.py
@@ -0,0 +1,33 @@
+# how do we deal with the input of split-seq?
+import os
+import subprocess
+import sys
+
+import pandas as pd
+import scipy.io
+from scipy import sparse
+
+from . import data_stats
+
+def process_split_seq(path):
+    """
+    Modifies the data so that it can be used in the uncurl_app pipeline.
+    """
+    # 1. cp DGE.mtx data.mtx
+    subprocess.call(['cp', os.path.join(path, 'DGE.mtx'), os.path.join(path, 'data.mtx')])
+    # 2. gzip data.mtx
+    subprocess.call(['gzip', os.path.join(path, 'data.mtx')])
+    # 3. load genes.csv, write out gene_names.txt
+    genes = pd.read_csv(os.path.join(path, 'genes.csv'))
+    gene_names = genes.gene_names
+    gene_names.to_csv(os.path.join(path, 'gene_names.txt'), header=None, index=None)
+    # 4. run the data_stats stuff
+    summary = data_stats.Summary(path, path, is_gz=True, shape='cell_gene')
+    script, div = summary.visualize()
+    summary.preprocessing_params()
+
+def add_cell_metadata_color_track(path):
+    # create a color track from the round 1 wells in cell_metadata.csv
+    # TODO
+    cell_metadata = pd.read_csv('cell_metadata.csv')
+    round1_wells = cell_metadata.rnd1_well
