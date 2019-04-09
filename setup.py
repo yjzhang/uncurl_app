eb6e3e2e18a39aea7ca4a2b69393ac437ab41b9e
Yue Zhang <yjzhang@cs.washington.edu>
2019-04-08 18:46:36 -0700

    move js and css to separate files

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(
     ],
     install_requires=install_requires,
     packages=find_packages("."),
+    include_package_data=True,
     test_suite='nose.collector',
     tests_require=['nose', 'flaky'],
     classifiers=[
