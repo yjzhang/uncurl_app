62775081606a054843ba52b1f566b9e4cc855d77
yjzhang <yjzhang@cs.washington.edu>
2019-10-01 23:05:43 +0000

    limited memory gunicorn script

---
+++ start_gunicorn_limited_memory.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+killall -9 redis-server
+nohup redis-server > nohup_redis.out &
+
+# kill all gunicorn processes at port 8888
+pids=`ps ax | grep gunicorn | grep "8888" | awk '{split($0,a," "); print a[1]}'`
+for pid in $pids; do
+    kill -9 $pid
+    echo "killed gunicorn process $pid"
+done
+# 20000 second timeout
+nohup gunicorn --workers 3 --max-requests 5 --threads 3 --bind 0.0.0.0:8888 --log-level debug -t 20000 wsgi:app >> nohup_uncurl_2.out &
