3aa40d036fb1e7b9a7864bec88fc0dbe6ccf78fd
Yue Zhang <yjzhang@cs.washington.edu>
2020-01-27 20:11:21 -0800

    added gene lists view

--- uncurl_app/db_query.py
+++ uncurl_app/db_query.py
@@ -89,41 +89,66 @@ def get_cell_info():
     """
     # TODO
     db = request.form['database_select']
-    cell_type = request.form['cell_type']
+    if 'mode' in request.form:
+        mode = request.form['mode']
+    else:
+        mode = 'gene_papers'
     try:
         if db == 'cellmarker':
-            test_type = request.form['test_type']
+            cell_type = request.form['cellmarker_cell_type']
             cells_or_tissues = request.form['cells_or_tissues']
             species = request.form['species']
             import cellmarker
-            result = []
-            cell_types = [result[0]]
-        elif db == 'cellmesh':
-            test_type = request.form['mesh_test_type']
-            species = request.form['cellmesh_species']
-            import cellmesh
-            result = []
-        elif db == 'cellmesh_anatomy':
-            mesh_subset = request.form['anatomy_mesh_subset']
-            import cellmesh
-            result = cellmesh.hypergeometric_test(top_genes, return_header=True, db_dir=cellmesh.ANATOMY_DB_DIR,
-                    cell_type_subset=mesh_subset, species=species)
-            cell_types = [result[0]]
-        elif db == 'go':
-            from cellmesh import go_query
-            top_genes = [x.capitalize() for x in top_genes]
-            result = go_query.gene_set_query(top_genes, return_header=True)
-            for r in result[1:]:
-                r[3] = ', '.join(r[3])
-            return json.dumps(result, cls=SimpleEncoder)
-        for i in range(1, min(20, len(result))):
-            ri = result[i]
-            gene_pmids = []
-            genes = ri[3]
+            genes = cellmarker.get_cell_genes(cell_type, cells_or_tissues=cells_or_tissues, species=species)
+            papers = []
             for g in genes:
-                gene_pmids.append('{0}: {1}'.format(g, ', '.join(pmid_to_link(x) for x in ri[4][g])))
-            cell_types.append((ri[0], ri[1], ri[2], ', '.join(ri[3]), ', '.join(gene_pmids)))
-        return json.dumps(cell_types, cls=SimpleEncoder)
+                pmids = cellmarker.get_papers_cell_gene(cell_type, g, species=species)
+                papers.append([cell_type, g, ', '.join([pmid_to_link(p) for p in pmids])])
+            if mode == 'gene_papers':
+                header = ['Cell type', 'Gene', 'PMIDs']
+                result = [header] + papers #[[cell_type, ', '.join(genes), '']]
+            else:
+                result = [['Genes']] + [[x] for x in genes]
+        elif db == 'cellmesh':
+            cell_type = request.form['cellmesh_cell_type']
+            species = request.form['cellmesh_species']
+            threshold = int(request.form['cellmesh_threshold']) - 1
+            import cellmesh
+            cell_id = cellmesh.get_cell_id_from_name(cell_type)
+            genes = cellmesh.get_cell_genes_pmids(cell_id, species=species, threshold=threshold)
+            header = ['Gene', 'PMIDs']
+            gene_papers = []
+            for gene, pmids in genes:
+                pmids = pmids.split(',')
+                gene_papers.append([gene, ', '.join([pmid_to_link(p) for p in pmids])])
+            if mode == 'gene_papers':
+                result = [header] + gene_papers
+            else:
+                result = [['Genes']] + [[g] for g, _ in genes]
+        elif db == 'cellmesh_anatomy':
+            cell_type = request.form['cellmesh_anatomy_cell_type']
+            species = request.form['cellmesh_anatomy_species']
+            threshold = int(request.form['cellmesh_anatomy_threshold']) - 1
+            import cellmesh
+            cell_id = cellmesh.get_cell_id_from_name(cell_type, db_dir=cellmesh.ANATOMY_DB_DIR)
+            genes = cellmesh.get_cell_genes_pmids(cell_id, species=species, threshold=threshold, db_dir=cellmesh.ANATOMY_DB_DIR)
+            header = ['Gene', 'PMIDs']
+            gene_papers = []
+            for gene, pmids in genes:
+                pmids = pmids.split(',')
+                gene_papers.append([gene, ', '.join([pmid_to_link(p) for p in pmids])])
+            if mode == 'gene_papers':
+                result = [header] + gene_papers
+            else:
+                result = [['Genes']] + [[g] for g, _ in genes]
+        elif db == 'kegg':
+            cell_type = request.form['kegg_cell_type']
+            species = request.form['kegg_species']
+            import kegg_query
+            genes = kegg_query.get_cell_genes(cell_type, species=species)
+            genes = genes[0]
+            result = [['Genes']] + [[g] for g in genes]
+        return json.dumps(result, cls=SimpleEncoder)
     except Exception as e:
         import traceback
         text = traceback.format_exc()
@@ -152,3 +177,37 @@ def get_mesh_tree():
             'id_to_name': id_to_name
     }
     return json.dumps(results_dict, cls=SimpleEncoder)
+
+@db_query.route('/db_query_genes')
+def db_query_genes():
+    """
+    View for getting gene lists
+    """
+    cell_queries = ['cellmarker', 'cellmesh', 'cellmesh_anatomy', 'kegg']
+    cell_lists = {}
+    for q in cell_queries:
+        cell_lists[q] = get_all_cell_types(q)
+    return render_template('db_query_genes.html', cell_lists=cell_lists)
+
+def get_all_cell_types(query, species='all'):
+    """
+    returns a list of all cell types for the given db.
+    Options: 'cellmarker', 'cellmesh', 'cellmesh_anatomy', 'kegg'
+    (not GO because there are plenty of other tools for GO)
+
+    species only matters for kegg, not cellmarker or cellmesh (which share cell types between species)
+    """
+    if query == 'cellmarker':
+        import cellmarker
+        cells = cellmarker.get_all_cells()
+    elif query == 'cellmesh':
+        import cellmesh
+        cells = [x[1] for x in cellmesh.get_all_cell_id_names(include_cell_components=False)]
+    elif query == 'cellmesh_anatomy':
+        import cellmesh
+        cells = [x[1] for x in cellmesh.get_all_cell_id_names(db_dir=cellmesh.ANATOMY_DB_DIR, include_cell_components=False)]
+    elif query == 'kegg':
+        import kegg_query
+        cells = kegg_query.get_all_cells(species=species)
+    cells.sort()
+    return cells
