1d688974a06805339474c3010a1b4bae4420d422
Yue Zhang <yjzhang@cs.washington.edu>
2020-03-03 00:59:21 -0800

    set default query method for cellmesh to be probabilistic model

--- start_gunicorn_docker.sh
+++ start_gunicorn_docker.sh
@@ -4,4 +4,4 @@ killall -9 redis-server
 nohup redis-server > nohup_redis.out &
 
 killall -9 gunicorn
-gunicorn --workers 4 --threads 1 --bind 0.0.0.0:8888 --log-level debug -t 20000 wsgi:app
+gunicorn --workers 4 --threads 1 --max-requests 5 --bind 0.0.0.0:8888 --log-level debug -t 20000 wsgi:app
